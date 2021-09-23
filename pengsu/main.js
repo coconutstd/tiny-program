a1a83d0f7339143625dae4091db0817625240e43
junui <wnsspdl@naver.com>
2021-09-23 22:26:32 +0900

    CI/CD와 애니메이션을 위한 점검

--- pengsu/main.js
+++ pengsu/main.js
@@ -1,27 +1,33 @@
 const URL = "https://xvqtmkgki7.execute-api.us-east-1.amazonaws.com/default/departAlarm"
 
+async function sendMessageEvent() {
+    let result = confirm('출발 메세지를 보내겠나?');
+    if (result) {
+        if(localStorage.getItem('isPengsu') === 'True'){
+            alert('이미 메세지를 보내셨습니다');
+        } else {
+            localStorage.setItem('isPengsu', 'True');
+            const res = await fetch(URL, {
+                method: "GET"
+            });
+            if (res.status === 200) {
+                alert('성공적으로 메세지를 보냈습니다');
+            } else {
+                alert('뭔가 오류가...')
+            }
+        }
+    } else {
+        console.log('실패');
+    }
+}
+
+function errorMessage() {
+    alert('나중에 오픈한다!');
+}
+
 function main() {
     const pengsu = document.getElementById('pengsu');
-    pengsu.addEventListener('click', async () => {
-        let result = confirm('출발 메세지를 보내겠나?');
-        if (result) {
-            if(localStorage.getItem('isPengsu') === 'True'){
-                alert('이미 메세지를 보내셨습니다');
-            } else {
-                localStorage.setItem('isPengsu', 'True');
-                const res = await fetch(URL, {
-                    method: "GET"
-                });
-                if (res.status === 200) {
-                    alert('성공적으로 메세지를 보냈습니다');
-                } else {
-                    alert('뭔가 오류가...')
-                }
-            }
-        } else {
-            console.log('실패');
-        }
-    })
+    pengsu.addEventListener('click', errorMessage);
 }
 
 window.addEventListener("load", function(event) {
